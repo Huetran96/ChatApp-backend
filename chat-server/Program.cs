06c0ad89e99665ec854ac864bbe960fb9d281f00
HÙNG ĐINH TUẤN <hungtuandinhbg@gmail.com>
2024-10-27 13:31:25 +0700

    add-chat-hub-service-midlleware

--- chat-server/Program.cs
+++ chat-server/Program.cs
@@ -10,7 +10,7 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDefaultCors();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -88,9 +88,13 @@ app.UseCors(options =>
 
 app.UseHttpsRedirection();
 
+app.UseCors();
+
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHub<ChatHub>("/chat-hub");
 
 app.Run();
