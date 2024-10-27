4fa1a9c8a756eb5d1ae18363c655c0c09da128de
HÙNG ĐINH TUẤN <hungtuandinhbg@gmail.com>
2024-10-27 13:29:14 +0700

    add-main-chat-hub

--- chat-server/Hubs/ChatHub.cs
+++ chat-server/Hubs/ChatHub.cs
@@ -1,6 +1,35 @@
 ﻿namespace chat_server.Hubs
 {
-    public class ChatHub
+    public class ChatHub(AppDbContext context) : Hub
     {
+        public static Dictionary<string, Guid> Users = new();
+
+        public async Task Connect(Guid userId)
+        {
+            Users.Add(Context.ConnectionId, userId);
+            User? user = await context.Users.FindAsync(userId);
+            if (user is not null)
+            {
+                user.Status = "online";
+                await context.SaveChangesAsync();
+
+                await Clients.All.SendAsync("Users", user);
+            }
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            Guid userId;
+            Users.TryGetValue(Context.ConnectionId, out userId);
+            Users.Remove(Context.ConnectionId);
+            User? user = await context.Users.FindAsync(userId);
+            if (user is not null)
+    {
+                user.Status = "offline";
+                await context.SaveChangesAsync();
+
+                await Clients.All.SendAsync("Users", user);
+            }
+        }
     }
 }
