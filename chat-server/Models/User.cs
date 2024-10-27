d277f782ebc608571eee7d98b82fc5e4fa02b808
HÙNG ĐINH TUẤN <hungtuandinhbg@gmail.com>
2024-10-27 13:48:24 +0700

    change-user-model

--- chat-server/Models/User.cs
+++ chat-server/Models/User.cs
@@ -4,7 +4,7 @@ namespace chat_server.Models
 {
     public class User : IdentityUser
     {
-        public bool Status {  get; set; }
+        public string Status {  get; set; }
         public string? Otp {  get; set; }
 
     }
