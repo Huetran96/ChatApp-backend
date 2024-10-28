803793af6a18d022f9f9c9c6bb31b454a148dc61
Huetran96 <huetranhh0507@gmail.com>
2024-10-28 10:30:12 +0700

    'merge-main'

--- chat-server/obj/Debug/net8.0/chat-server.AssemblyInfo.cs
+++ chat-server/obj/Debug/net8.0/chat-server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("chat-server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+159cce95163859af78d054329846b3f28bf489f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96107eca2eedc01ebbb5a75d58b16adee7155454")]
 [assembly: System.Reflection.AssemblyProductAttribute("chat-server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("chat-server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
